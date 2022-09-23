d9012f850344a5839328531789ab070f9d6dd209
Gus180474 <gustep_ebc2001@yahoo.com.mx>
2022-09-23 17:09:19 -0500

    Lista de productos: HTML a partir de arrays

--- main1.js
+++ main1.js
@@ -4,6 +4,7 @@ const menuHamIcon = document.querySelector('.menu');
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const mobileMenu = document.querySelector('.mobile-menu');
 const aside = document.querySelector('.product-detail');
+const cardsConteiner = document.querySelector('.cards-container');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
@@ -34,3 +35,85 @@ function toggleCarritoAside() {
     }
     aside.classList.toggle('inactive');
 }
+/*
+    <div class="product-card">
+      <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
+      <div class="product-info">
+        <div>
+          <p>$120,00</p>
+          <p>Bike</p>
+        </div>
+        <figure>
+          <img src="./icons/bt_add_to_cart.svg" alt="">
+        </figure>
+      </div>
+    </div>
+
+
+*/
+
+const productList = [];
+productList.push({
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+productList.push({
+    name: 'Pantalla',
+    price: 1200,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+productList.push({
+    name: 'Compu',
+    price: 15500,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+
+// se puede ingresar en una funcion el recorrido del array de acuerdo al tipo de producto
+// function rebderProducts(arr){}
+for (product of productList){
+    const productCard = document.createElement('div');
+    productCard.classList.add('product-card');
+
+    const productImg = document.createElement('img');
+    productImg.setAttribute('src', product.image);
+    // product={name,price, image} ->se utiliza product.image para crear cada una de las imagenes
+
+    const productInfo = document.createElement('div');
+    productInfo.classList.add('product-info');
+
+    const productInfoDiv = document.createElement('div');
+
+    const productPrice = document.createElement('p');
+    productPrice.innerText = '$' + product.price;
+    const productName = document.createElement('p');
+    productName.innerText = product.name;
+/*
+    productInfoDiv.appendChild(productPrice);
+    productInfoDiv.appendChild(productName);
+*/
+    productInfoDiv.append(productPrice, productName);
+
+    const productInfoFigure = document.createElement('figure');
+    const productImgCart = document.createElement('img');
+    productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+
+    productInfoFigure.appendChild(productImgCart);
+/*
+    productInfo.appendChild(productInfoDiv);
+    productInfo.appendChild(productInfoFigure);
+*/
+    productInfo.append(productInfoDiv, productInfoFigure);
+
+
+/*
+    productCard.appendChild(productImg);
+    productCard.appendChild(productInfo);
+*/
+    productCard.append(productImg, productInfo);
+
+    cardsConteiner.appendChild(productCard);
+}
+
+// se manda allamar la funcion de acuerdo al tipo de producto al que se requiera hacer el recorrido
+// rebderProducts(productList);
